3b59ac4bd8cab78f2b67bff85c439fba3183b034
mariavarand-ship-it <mariavarand@gmail.com>
2026-05-26 17:25:02 +0300

    Update service-worker.js

--- service-worker.js
+++ service-worker.js
@@ -1,5 +1,4 @@
-const CACHE_NAME = "nuzhnye-slova-v4";
-
+const CACHE_NAME = "nuzhnye-slova-v5";
 const FILES_TO_CACHE = [
   "./",
   "./index.html",
@@ -8,15 +7,12 @@ const FILES_TO_CACHE = [
   "./manifest.json",
   "./apple-touch-icon.png"
 ];
-
 self.addEventListener("install", event => {
   event.waitUntil(
     caches.open(CACHE_NAME).then(cache => cache.addAll(FILES_TO_CACHE))
   );
-
   self.skipWaiting();
 });
-
 self.addEventListener("activate", event => {
   event.waitUntil(
     caches.keys().then(keys =>
@@ -25,26 +21,24 @@ self.addEventListener("activate", event => {
           if (key !== CACHE_NAME) {
             return caches.delete(key);
           }
-
           return null;
         })
       )
     )
   );
-
   self.clients.claim();
 });
-
 self.addEventListener("fetch", event => {
+  if (event.request.method !== "GET") {
+    return;
+  }
   event.respondWith(
     fetch(event.request)
       .then(response => {
         const responseClone = response.clone();
-
         caches.open(CACHE_NAME).then(cache => {
           cache.put(event.request, responseClone);
         });
-
         return response;
       })
       .catch(() => caches.match(event.request))
